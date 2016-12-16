1b31e55914d185943594a7edbc2a928280d30dea
Vitaliy Kuzin <saynschuman@gmail.com>
2016-12-16 18:05:04 +0200

    friday night

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
-	/*MOBILE MENU BUTTON*/
+	//	mobile button menu
 	$('.mobile-menu').click(function() {
 		$(this).toggleClass('active');
 	});
+
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', 'libs/particles.json', function() {
+	  
+	});	
+
+	if (innerHeight < 806) {
+		$('.main_screen').height(innerHeight);
+	}
+
+	$(window).resize(function(){
+		if (innerHeight < 806) {
+			$('.main_screen').height(innerHeight);
+		}
+	})
 
 
 });
