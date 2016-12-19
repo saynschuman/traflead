b9eadf55b222e6119e2aa01cfd22e935bf62ed7f
Vitaliy Kuzin <saynschuman@gmail.com>
2016-12-19 17:47:06 +0200

    main template finished

--- src/js/main.js
+++ src/js/main.js
@@ -24,22 +24,13 @@ $(document).ready(function() {
 		var titleImgWidth = $('.title img').width();
 		$('.main_screen .offert').css('max-width', titleImgWidth);	
 	})
+	
+	
+    
+        
+     
 
-	// smooth scroll
 
-	$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+
 
 });
