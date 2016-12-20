11bd4fd1faee382ed14d31375102c1431d220bc9
Vitaliy Kuzin <saynschuman@gmail.com>
2016-12-20 20:05:21 +0200

    last map finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -199,6 +199,13 @@ $(document).ready(function() {
 	  }
 	);		
 
-
+smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+    selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+    speed: 1500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeOutQuart', // Easing pattern to use
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+});
 
 });
